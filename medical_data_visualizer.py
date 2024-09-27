13c283c32da74a28a81a21264cb41eaf17877b54
Gustavo Colombini <gucolombini@gmail.com>
2024-09-27 19:41:28 -0300

    feat: adiciona função funcional de geração dos gráficos

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
 
-# 2
-df['overweight'] = None
+df = pd.read_csv('medical_examination.csv')
 
-# 3
+# Criação da coluna overweight e normalização dos dados
+df['overweight'] = df.apply(lambda row: 1 if row['weight'] / ((row['height']/100) ** 2) > 25 else 0, axis=1)
+df['cholesterol'] = df.apply(lambda row: 1 if row['cholesterol'] > 1 else 0, axis=1)
+df['gluc'] = df.apply(lambda row: 1 if row['gluc'] > 1 else 0, axis=1)
 
-
-# 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Cria df que separa todas as variáveis em relação à coluna cardio, e depois conta a quantidade em cada grupo
+    df_cat = df.melt(id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index=False).value_counts().rename(columns={'count': 'total'})
 
-
-    # 6
-    df_cat = None
-    
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # Gera e salva o gráfico
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar').figure
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Limpeza dos dados incorretos de acordo com as instruções
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) &
+                 (df['height'] >= df['height'].quantile(0.025)) & 
+                 (df['height'] <= df['height'].quantile(0.975)) &
+                 (df['weight'] >= df['weight'].quantile(0.025)) &
+                 (df['weight'] <= df['weight'].quantile(0.975))]
+    
+    # Gera a matriz de correlação
+    corr = df_heat.corr()
 
-    # 12
-    corr = None
-
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    # Gera gráfico com máscara para o triângulo superior da matriz e salva ele
+    fig, ax = plt.subplots(figsize=(12, 8))
+    mask = np.triu(corr)
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', vmax=0.3, vmin=-0.1, center=0, square=True, linewidths=0.5)
     fig.savefig('heatmap.png')
     return fig
